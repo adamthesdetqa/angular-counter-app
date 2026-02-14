2b948d25bd273e03c52c1f053dd5af9676a628bb
Adam Bethlehem <adam.bethlehem@safetyio.com>
2026-02-14 09:39:47 +0200

    Different ways to do do conditionals and conditional syles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('counter-app');
+  count = signal(0);
+
+  increment() {
+    this.count.update((c) => ++c);
+  }
+  decrement() {
+    this.count.update((c) => --c);
+  }
+
+  getCounterStatus(): string {
+    if (this.count() > 0) {
+      return 'positive';
+    } else if (this.count() < 0) {
+      return 'negative';
+    } else {
+      return 'zero';
+    }
+  }
 }
+// use ngSwitch to show if positive or negatove
